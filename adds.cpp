4505d27af85b01c45f6c5a2e03849ac24f9ebc52
Enatalina <liz.nataljina@yandex.ru>
2021-12-27 03:29:43 +0300

    adds refreshed

--- adds.cpp
+++ adds.cpp
@@ -41,17 +41,36 @@ void adds::on_tableView_pressed(const QModelIndex &index)
 
 void adds::on_pushButton_clicked()
 {
-    QSqlQuery query;
-    query.prepare("INSERT INTO liza_and_egor.courses_users VALUES ('"+chosen+"', '" + login + "');");
-    QSqlQuery query1;
-    query1.prepare("UPDATE liza_and_egor.courses SET amount=amount+1 WHERE title='"+chosen+"'");
+    QSqlQuery query1, query;
+    query.prepare("SELECT * FROM liza_and_egor.users WHERE login='"+login+"'");
+    query1.prepare("SELECT cost FROM liza_and_egor.courses WHERE title='"+chosen+"'");
+    query.exec();
+    query.next();
+    query1.exec();
+    query1.next();
     if (ui->pushButton->isEnabled())
     {
-        query.exec();
-        query1.exec();
-        qDebug() << login << "  " << chosen;
-        qDebug() << m_model->lastError();
+        qDebug() << query.value(7).toString().remove(0,1).toFloat();
+        qDebug() << query1.value(0).toString().remove(0,1).toFloat() <<  query1.value(0).toString().remove(0,1);
+        QString tmp =  query1.value(0).toString().remove(0,1);
+        if(tmp.toFloat() > query.value(7).toString().remove(0,1).toFloat()){
+            ui->out->setText("У вас нет столько денег...");
+            return;
+        }
+        else{
+        query1.prepare("UPDATE liza_and_egor.courses SET amount=amount+1 WHERE title='"+chosen+"'");
+        m_model->setQuery("INSERT INTO liza_and_egor.courses_users VALUES ('"+chosen+"', '" + login + "');");
+        if (!m_model->lastError().isValid()){
+            query1.exec();
+            query.prepare("UPDATE liza_and_egor.users SET bank = bank - '" + tmp + "' WHERE login='"+login+"'");
+            query.exec();
+        }
+        else{
+            ui->out->setText("Вы уже купили этот курс.");
+            return;
+        }
         accept();
+        }
     }
 }
 
