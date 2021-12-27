1f906a6c19b108010974a0dd5aa3285cc6ead7d0
Enatalina <liz.nataljina@yandex.ru>
2021-12-27 03:26:40 +0300

    adds refreshed

--- student.cpp
+++ student.cpp
@@ -32,6 +32,7 @@ void student::on_pushButton_clicked()
     if (dialog->exec() == QDialog::Accepted)
     {
         m_model->setQuery(" SELECT title, description, (surname || ' ' || name) AS ФИО_Преподавателя FROM liza_and_egor.users JOIN liza_and_egor.courses ON login = tutor JOIN liza_and_egor.courses_users ON title = course WHERE username = '"+ login + "';");
+        update();
     }
 }
 
@@ -50,10 +51,19 @@ void student::update(){
 
 void student::on_pushButton_3_clicked()
 {
-    if (dialog_pay->exec() == QDialog::Accepted)
+    if (dialog_ch->exec() == QDialog::Accepted)
     {
         update();
     }
 }
 
+
+
+void student::on_pushButton_2_clicked()
+{
+    if (dialog_pay->exec() == QDialog::Accepted)
+    {
+        update();
+    }
+}
 
